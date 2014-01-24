9be548ce67edd5d794c9d65c895138db27cc2d10
Mark Louie Almeda <mlb.almeda@gmail.com>
2014-01-25 02:53:41 +0800

    Jan 25 End daily of development

--- Admin/js/cmsjs.js
+++ Admin/js/cmsjs.js
@@ -12,3 +12,32 @@ $(function() {
     
 //    $( "ul, li" ).disableSelection();
 });
+
+
+$(document).ready(function(){
+    $("#editPanel #header li").click(function(){
+        $("#editPanel #header li").removeClass("selected");
+        $(this).addClass("selected");
+        
+        $("#editPanel #content #overflow").animate({"margin-left": -$(this).index() * 100 + "%"});
+    });
+    
+    
+    $("#editPanel #header>#toggle").click(function(){
+
+        
+        if($(this).html() == 4)
+        {
+            $("#editPanel #content").css({"height":"0px"}); //.css({"bottom":"-245px"});
+            $("#editPanel").css({"bottom":"-245px"});
+            $(this).html('3');
+        }
+        else
+        {
+            $("#editPanel #content").css({"height":"245px"});//css({"bottom":"0px"});
+            $("#editPanel").css({"bottom":"0px"});
+            $(this).html(4);
+        }
+        
+    });
+});
